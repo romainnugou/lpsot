5f109832ec2462ec5e69ed29ba2844ea7bacf491
Romain Nugou <romain.nugou@logient.com>
2024-11-23 11:27:42 -0500

    Add mixed-content meta

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <title>LPSOT</title>
   <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet">
   @vite('resources/css/app.css')
